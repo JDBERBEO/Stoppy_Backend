b076fd636fb5e5b1650fd72a0bd9155d03b87fac
Juan <jd.berbeo@gmail.com>
2021-08-05 08:22:47 -0500

    crea port, e instala socket io client

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const SocketIO = require('socket.io')
-const cors = require('cors')
+const http = require('http')
 
 const port = 8000
+const app = express()
+const server = http.createServer(app)
+const io = SocketIO(server, {
+  cors: {
+    origin: '*'
+  }
+})
 
+//escuchar por los eventos 
+io.on('connection', socket => {
+socket.emit('welcome', { message: 'Bienvenido'})
+})
+app.get('/', () => {
+  console.log('hola mundo')
+})
+
+server.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`)
+})
