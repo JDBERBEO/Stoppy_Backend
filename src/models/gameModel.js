14db95e35eb206e4a51f95aff7631912811501e5
Juan <jd.berbeo@gmail.com>
2021-08-11 13:13:54 -0500

    crea modelo de juego

---
+++ src/models/gameModel.js
@@ -0,0 +1,29 @@
+const { Schema, model, models } = require("mongoose");
+
+const playerSchema = new Schema( 
+    {
+    letter: {
+    type: [String],
+    },
+    name: {
+        type: [String],
+    },
+    place : {
+      type: [String],
+    },
+    fruit: {
+        type: [String],
+    },
+    color: {
+        type: [String],
+    },
+    thing: {
+        type: [String],
+    },
+    totalScorePerRound: {
+        type: [Number]
+    }
+  })
+
+const Game = model('Game', playerSchema)
+module.exports = Game
