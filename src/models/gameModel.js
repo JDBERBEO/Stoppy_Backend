857babf765ee8fbd4b9917972b51ae9c796727b3
Juan David <jd.berbeo@gmail.com>
2021-08-13 21:16:52 -0500

    modifica modelo de juego y elimina las propeidades de respuestas

--- src/models/gameModel.js
+++ src/models/gameModel.js
@@ -3,27 +3,7 @@ const { Schema, model, models } = require("mongoose");
 const gameSchema = new Schema( 
     {
     players: [{type: Schema.Types.ObjectId, ref: "Player"}],
-    letter: {
-    type: [String],
-    },
-    name: {
-        type: [String],
-    },
-    place : {
-      type: [String],
-    },
-    fruit: {
-        type: [String],
-    },
-    color: {
-        type: [String],
-    },
-    object: {
-        type: [String],
-    },
-    totalScorePerRound: {
-        type: [Number]
-    }
+
   })
 
   
